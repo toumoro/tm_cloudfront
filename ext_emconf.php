c508f97e0634f051cb46d7e07ac255f372673ce9
Stéphane Franic <stephane.franic@toumoro.com>
2021-01-15 17:15:06 -0500

    Support TYPO3 9.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -23,7 +23,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-8.7.99',
+            'typo3' => '8.7.0-9.5.99',
             'aws' => '*',
         ],
         'conflicts' => [],
