eb37b944e7fee3e798ce1a0ba20e8b8a2d941d28
mmunos <marc.munos@mce.gouv.qc.ca>
2023-02-01 12:48:47 -0500

    whitespaces

--- Classes/Hooks/ClearCachePostProc.php
+++ Classes/Hooks/ClearCachePostProc.php
@@ -201,15 +201,15 @@ class ClearCachePostProc
         }
 
         if (MathUtility::canBeInterpretedAsInteger($entry)) {
-             $languages = GeneralUtility::makeInstance(SiteFinder::class)->getSiteByPageId($pageId)->getAllLanguages();
-    
+            $languages = GeneralUtility::makeInstance(SiteFinder::class)->getSiteByPageId($pageId)->getAllLanguages();
+
             if (count($languages) > 0) {
-               $this->enqueue($this->buildLink($entry, array('_language' => 0)) . $wildcard, $distributionIds);
+                $this->enqueue($this->buildLink($entry, array('_language' => 0)) . $wildcard, $distributionIds);
                 foreach ($languages as $k => $lang) {
-                   if($lang->getLanguageId() != 0) {
-                       $this->enqueue($this->buildLink($entry, array('_language' => $lang->getLanguageId())) . $wildcard, $distributionIds);                                                            
-                   }                                                                   
-               }
+                    if($lang->getLanguageId() != 0) {
+                        $this->enqueue($this->buildLink($entry, array('_language' => $lang->getLanguageId())) . $wildcard, $distributionIds);
+                    }
+                }
             } else {
                 $this->enqueue($this->buildLink($entry) . $wildcard, $distributionIds);
             }
